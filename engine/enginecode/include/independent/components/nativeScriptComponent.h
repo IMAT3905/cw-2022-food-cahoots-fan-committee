e8d2fcb8fac6fca3a0bedb726f8e7d3dc2218c19
Daniel Gardener <P2529371@my365.dmu.ac.uk>
2022-02-02 13:57:58 +0000

    Created a native script component.

---
+++ engine/enginecode/include/independent/components/nativeScriptComponent.h
@@ -0,0 +1,40 @@
+/* \file nativeScriptComponent.h */
+#pragma once
+
+#include <entt/entt.hpp>
+#include "events/events.h"
+#include <glm/gtc/type_ptr.hpp>
+
+namespace Engine
+{
+	class NativeScript
+	{
+	public:
+		NativeScript() : m_entity(entt::null) {};
+		NativeScript(entt::entity& entity) : m_entity(entity) {};
+		virtual ~NativeScript() = default;
+		virtual void onCreate() {};
+		virtual void onDestroy() {};
+		virtual void onUpdate(float timestep);
+		virtual void onKeyPress(KeyPressedEvent& e) {};
+	protected:
+		entt::entity m_entity;
+	};
+
+	class NativeScriptComponent
+	{
+	public:
+		NativeScriptComponent() = default;
+		~NativeScriptComponent() = default;
+		void onUpdate(float timestep) { m_instance->onUpdate(timestep); };
+		void onKeyPress(KeyPressedEvent& e) { m_instance->onKeyPress(e); };
+		template <typename G, typename ...Args> void create(Args&& ... args)
+		{
+			G* ptr = new G(std::forward<Args>(args) ...);
+			m_instance.reset(static_cast<NativeScript*>(ptr));
+			m_instance->onCreate();
+		}
+	protected:
+		std::shared_ptr<NativeScript> m_instance = nullptr;
+	};
+}
