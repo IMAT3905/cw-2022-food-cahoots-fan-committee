337388ac9928e15a69bb06b046bf0a81fb817e46
Volity2001 <edwardlammasgames@gmail.com>
2022-02-16 00:06:21 +0000

    finished most of the code

--- engine/enginecode/src/independent/UI/Containers.cpp
+++ engine/enginecode/src/independent/UI/Containers.cpp
@@ -13,4 +13,60 @@ namespace Engine {
 	void RootContainer::OnRender() const {
 		for (auto& container : m_containers) container.OnRender();
 	}
+
+	void RootContainer::AddContainer(HorizontalContainer& container) {
+		container.SetParent(this);
+		container.m_colourscheme = m_colourscheme;
+		for (auto& widget : container) { widget->m_colourscheme = m_colourscheme; }
+		m_containers.push_back(container);
+		UpdateSize();
+	}
+
+	void RootContainer::UpdatePosition(glm::ivec2& relativepos) {
+		m_position = relativepos;
+		glm::ivec2 childrelativepos = m_position + glm::ivec2(ModalWindow::padding, ModalWindow::padding);
+		for (auto& container : m_containers) {
+			container.UpdatePosition(childrelativepos);
+			childrelativepos.y += container.GetSize().y;
+		}
+	}
+
+	void RootContainer::UpdateSize() {
+		glm::ivec2 size(0);
+		size += 2 * glm::ivec2(ModalWindow::padding, ModalWindow::padding);
+		for (auto& container : m_containers) {
+			size.x = std::max(size.x, ModalWindow::padding * 2 + container.GetSize().x);
+			size.y += container.GetSize().y;
+		}
+
+		m_size = size;
+
+		m_parent->UpdateSize();
+	}
+
+	void HorizontalContainer::UpdatePosition(glm::ivec2& relativepos) {
+		m_position = relativepos;
+
+		glm::ivec2 childrelativepos = m_position + glm::ivec2(ModalWindow::padding, ModalWindow::padding);
+		for (auto& widget : m_widgets) {
+			widget->UpdatePosition(childrelativepos);
+			childrelativepos.x += widget->GetSize().x;
+		}
+	}
+
+	void HorizontalContainer::UpdateSize() {
+		glm::ivec2 size(0);
+		size += 2 * glm::ivec2(ModalWindow::padding, ModalWindow::padding);
+		for (auto& widget : m_widgets) {
+			size.x += widget->GetSize().x;
+			size.y = std::max(size.y, ModalWindow::padding * 2 + widget->GetSize().y);
+		}
+
+		m_size = size;
+		if (m_parent) m_parent->UpdateSize();
+	}
+
+	void HorizontalContainer::OnRender() const {
+		for (auto widget : m_widgets) widget->OnRender();
+	}
 }
