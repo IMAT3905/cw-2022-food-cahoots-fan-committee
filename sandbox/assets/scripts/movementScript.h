ca4fe8f16643c74ce9f358555963f0769b9567f3
Daniel Gardener <dgardener2001@gmail.com>
2022-03-30 01:05:38 +0100

    Improved how the conveyor belt objects are created and updated the movement script.

--- sandbox/assets/scripts/movementScript.h
+++ sandbox/assets/scripts/movementScript.h
@@ -12,7 +12,7 @@ struct MovementKeyFrames
 class MovementScript : public Engine::NativeScript
 {
 public:
-	MovementScript(entt::entity& entity, uint32_t startPoint);
+	MovementScript(entt::entity& entity, float startT) : NativeScript(entity), t(startT) {};
 	virtual void onCreate() override;
 	virtual void onUpdate(float timestep) override;
 	glm::vec3 lerp(glm::vec3 a, glm::vec3 b, float t);
@@ -20,32 +20,23 @@ public:
 private:
 	entt::registry& m_registry = Engine::Application::getInstance().getRegistry();
 	float speed = 0.1;
-	MovementKeyFrames keyFrames[17];
+	MovementKeyFrames keyFrames[9];
 	glm::vec3 pos;
 	float t = 0.f;
 	float height = 5.f;
 	float width = 5.f;
 	float radius = 0.5f;
-	uint32_t startPoint;
 	bool first = true;
 	float angle = 0.f;
-	glm::vec3 positions[16] =
+	glm::vec3 positions[8] =
 	{
 		glm::vec3(2.5f, 0.f, 3.f),
-		glm::vec3(2.5f - (width / 3.f), 0.f, 3.f),
-		glm::vec3(2.5f - width / 3.f * 2.f, 0.f, 3.f),
 		glm::vec3(2.5f - width, 0.f, 3.f),
 		glm::vec3(2.5f - width - radius, 0.f, 3.f - radius),
-		glm::vec3(2.5f - width - radius, 0.f, 3.f - radius - height / 3.f),
-		glm::vec3(2.5f - width - radius, 0.f, 3.f - radius - height / 3.f * 2.f),
 		glm::vec3(2.5f - width - radius, 0.f, 3.f - radius - height),
 		glm::vec3(2.5f - width, 0.f, 3.f - radius - height - radius),
-		glm::vec3(2.5f - width + (width / 3.f), 0.f, 3.f - radius - height - radius),
-		glm::vec3(2.5f - width + (width / 3.f * 2.f), 0.f, 3.f - radius - height - radius),
 		glm::vec3(2.5f, 0.f, 3.f - radius - height - radius),
 		glm::vec3(2.5f + radius, 0.f, 3.f - height - radius),
-		glm::vec3(2.5f + radius, 0.f, 3.f - height + (height / 3.f) - radius),
-		glm::vec3(2.5f + radius, 0.f, 3.f - height + (height / 3.f * 2.f) - radius),
 		glm::vec3(2.5f + radius, 0.f, 3.f - radius),
 	};
 };
